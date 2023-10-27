33faba69613f8423f320d61f295d60ea6fbf2e2f
Vamsi Thiriveedhi <115020590+vkt1414@users.noreply.github.com>
2023-10-26 21:20:04 -0400

    Update preProcessNotebooks.py

--- preProcessNotebooks.py
+++ preProcessNotebooks.py
@@ -14,8 +14,8 @@ def modify_notebook(file_name):
             cell.source = cell.source.replace('GC_PROJECT_ID = "idc-external-030" # @param {type:"string"}\n', "#this project_id is modified by preprocessingNotebooks.py\nGC_PROJECT_ID=''")
             # Comment out another line
             cell.source = cell.source.replace('auth.authenticate_user()', '#while running the notebook with papermill, the authentication is handled by using application default credentials\n#auth.authenticate_user()')         
-            cell.source = cell.source.replace('/app', '/cromwell_root' + '/app')         
-            cell.source = cell.source.replace('/content', '/cromwell_root/content')  # Adjust this line to replace '/content' with '/cromwell_root/content'         
+            #cell.source = cell.source.replace('/app', '/cromwell_root' + '/app')         
+            #cell.source = cell.source.replace('/content', '/cromwell_root/content')  # Adjust this line to replace '/content' with '/cromwell_root/content'         
             cell.source = cell.source.replace('files.download(archive_fn)', '#while running the notebook with papermill, there is no need to download\n#files.download(archive_fn)')   
 
             # Check if the cell contains the specific string and add a tag
